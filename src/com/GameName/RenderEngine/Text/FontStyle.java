bb9d8bc9386cad09c54c8ca63396464863466125
Joshua <JCilfone@gmail.com>
2016-08-03 15:42:17 -0400

    Removed Font Code

--- src/com/GameName/RenderEngine/Text/FontStyle.java
+++
@@ -1,9 +0,0 @@
-package com.GameName.RenderEngine.Text;
-
-public enum FontStyle {
-	Plain, Bold, Italic, Bold_Italic;
-	
-	public static FontStyle getStyle(boolean bold, boolean italic) {
-		return bold && italic ? FontStyle.Bold_Italic : bold ? Bold : Italic;
-	}
-}
